d8739a7a9bcfdceb3f9b84ef58cec17db8205899
Mike Halsey <mhalsey0@gmail.com>
2025-07-22 17:55:57 -0400

    reorganizing files for ease of understanding

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,4 +1,7 @@
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("ApplicationDbContextConnection") ?? throw new InvalidOperationException("Connection string 'ApplicationDbContextConnection' not found.");
+
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connectionString));
 
 // Add services to the container.
 
